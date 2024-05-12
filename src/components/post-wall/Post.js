2a82eb935f844fb6b14052ec63d90b11eddd7fd7
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-05-12 11:15:19 +0300

    Added adaptability of image sizes depending on their number in one post

--- src/components/post-wall/Post.js
+++ src/components/post-wall/Post.js
@@ -1,15 +1,23 @@
 export function Post({post}) {
+
+    // Converting an array of image references to an array of image elements
     const images = post.images.map(image =>
         <img src={image} alt=""/>
     );
-
 
     return (
         <div className="post">
             <div className="content">
                 {post.text}
             </div>
-            <div className="images">
+            <div className={
+                [
+                    'images',
+                    images.length === 1 ? 'image-1' :
+                        (images.length === 2 ? 'image-2' :
+                            (images.length === 3 ? 'image-3' : ''))
+                ].join(' ')
+            }>
                 {images}
             </div>
             <p className="datetime">{post.datetime}</p>
