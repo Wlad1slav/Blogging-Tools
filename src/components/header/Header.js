86d3f425451f76e0085f4b6f21c2a0dc57650ae0
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-05-12 16:42:44 +0300

    Added the fixed header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,42 +1,30 @@
-import './style.scss';
+import {TopHeader} from "./TopHeader";
+import {FixedHeader} from "./FixedHeader";
+import {useEffect, useState} from "react";
 
 export function Header() {
+
+    const [showFixedHeader, setShowFixedHeader] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 350) {
+            setShowFixedHeader(true);
+        } else if (window.scrollY === 0) {
+            setShowFixedHeader(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <header>
-
-            <img src="/images/avatar.gif" alt="" className="avatar"/>
-
-            <div className="personal-info">
-                <h1>Rasik</h1>
-
-                <p className="blog-description">
-                    Буду тут писати свої нікому непотрібні думки, подалі від людських очів.
-                </p>
-
-                <div className="bio">
-                    <h2>Bio</h2>
-                    <p>
-                        Веб програміст з Криму, проживаю в Києві. Переконаний зрадойоб і монархіст, трошки християнин і дуже бі.
-                        Етнічний українець без домішок, але є мрія стати євреєм (чи хоча б знайти єврейські коріння).
-                    </p>
-
-                    <p>
-                       Support: 🇹🇼 🏴󠁩󠁱󠁫󠁲󠁿 🇮🇱 🇺🇸 🏳️‍🌈 🏳️‍⚧️
-                    </p>
-
-                    <p>
-                        KILL WITH FIRE: 🇸🇺 🇷🇺 🇨🇳 🏴󠁵󠁳󠁣󠁡󠁿
-                    </p>
-                </div>
-            </div>
-
-            <ul className="links">
-                <h2>Links</h2>
-                <li><a href="#">Github</a> </li>
-                <li><a href="#">Telegram</a> </li>
-                <li><a href="#">Post Backups</a> </li>
-            </ul>
-
-        </header>
+        <>
+            {/*{showFixedHeader ? <FixedHeader /> : <TopHeader /> }*/}
+            <FixedHeader isDisplayed={showFixedHeader} />
+            <TopHeader isDisplayed={!showFixedHeader} />
+        </>
     );
 }
---
+++ src/components/header/TopHeader.js
@@ -0,0 +1,41 @@
+import './styles/top.scss';
+
+export function TopHeader({isDisplayed}) {
+    return (
+        <header style={!isDisplayed ? {display: "none"} : {}}>
+            <img src="/images/avatar.gif" alt="" className="avatar"/>
+
+            <div className="personal-info">
+                <h1>Rasik</h1>
+
+                <p className="blog-description">
+                    Буду тут писати свої нікому непотрібні думки, подалі від людських очів.
+                </p>
+
+                <div className="bio">
+                    <h2>Bio</h2>
+                    <p>
+                        Веб програміст з Криму, проживаю в Києві. Переконаний зрадойоб і монархіст, трошки християнин і дуже бі.
+                        Етнічний українець без домішок, але є мрія стати євреєм (чи хоча б знайти єврейські коріння).
+                    </p>
+
+                    <p>
+                       Support: 🇹🇼 🏴󠁩󠁱󠁫󠁲󠁿 🇮🇱 🇺🇸 🏳️‍🌈 🏳️‍⚧️
+                    </p>
+
+                    <p>
+                        KILL WITH FIRE: 🇸🇺 🇷🇺 🇨🇳 🏴󠁵󠁳󠁣󠁡󠁿
+                    </p>
+                </div>
+            </div>
+
+            <ul className="links">
+                <h2>Links</h2>
+                <li><a href="#">Github</a> </li>
+                <li><a href="#">Telegram</a> </li>
+                <li><a href="#">Post Backups</a> </li>
+            </ul>
+
+        </header>
+    );
+}
