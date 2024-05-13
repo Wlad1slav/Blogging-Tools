077114d8c83f794655f68d548c6f3cf8be97c759
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-05-13 19:30:02 +0300

    Update routes.py
    - Permission to receive API requests from localhost
    - "/personal_information" returns a full object for convenience

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from models.personal_information import PersonalInformation
 
 """
@@ -12,8 +14,20 @@ Starting the router:
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allows all origins from localhost:3000
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 @app.get("/personal_information")
 async def get_personal_information():
+    result = {}
     personal_information = PersonalInformation()
-    return personal_information.read_all()
+    for setting in personal_information.read_all():
+        # A single dictionary is created from an array of dictionaries (strings from the database).
+        result[setting['key']] = setting['value']
+    return result
