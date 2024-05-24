db013351c560913f70d65dad35ecc8d25db69310
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-05-24 09:48:40 +0300

    Added API route to get one specific post

--- api/laravel/routes/api.php
+++ api/laravel/routes/api.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\API\PostsController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/posts', [PostsController::class, 'get']);
+Route::get('/posts', [PostsController::class, 'getAll']);
+Route::get('/post', [PostsController::class, 'get']);
 Route::post('/post/create', [PostsController::class, 'store']);
 
 Route::get('/personal_information', [PersonalInformationController::class, 'get']);
