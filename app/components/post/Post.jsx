14358dad1a17a1b4793f6b84195c5e15fec5724b
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-05-24 22:30:59 +0300

    Receiving a specific post by ID on a separate page, accessible by link when generating each post

--- app/components/post/Post.jsx
+++ app/components/post/Post.jsx
@@ -25,7 +25,7 @@ export function Post({post}) {
         <div className="post">
             <div className='head'>
                 <h3>{post.title}</h3>
-                <a href="/">{post.id}</a>
+                <a href={`/post/${post.id}`}>{post.id}</a>
             </div>
             <div className="content">
                 {content}
---
+++ app/post/[id]/page.js
@@ -0,0 +1,54 @@
+import React from "react";
+import axios from "axios";
+import Post from "@/app/components/post/Post";
+
+/**
+ * Fetch post data by ID
+ *
+ * @param id
+ * @returns {Promise<{id: string, title: string, text: string, created_at: string, images: Array<string>}>}
+ */
+const fetchPostData = async (id) => {
+    const apiRequest = `${process.env.NEXT_PUBLIC_API_BASE_URL}/post?id=${id}`;
+    try {
+        const response = await axios.get(apiRequest);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching post:", error);
+        return {
+            id,
+            title: "Post not found",
+            text: "",
+            created_at: "",
+            images: [],
+        };
+    }
+};
+
+/**
+ * Generation of page metadata. The post title serves as the title, if the post title does not exist,
+ * then the post text is truncated to 70 characters and this segment is set as the page title.
+ * The first 320 characters of the post text are set as the meta description.
+ *
+ * @param params
+ * @returns {Promise<{title: (string|*)}|{title: string}>}
+ */
+export async function generateMetadata({ params }) {
+    const post = await fetchPostData(params.id);
+
+    return {
+        title: post.title ?? (post.text.length > 70 ? post.text.substring(0, 70) : post.text),
+        description: post.text.length > 320 ? post.text.substring(0, 320) : post.text,
+    };
+}
+
+/**
+ * A page with a specific post
+ */
+export default async function PostPage({ params }) {
+    const post = await fetchPostData(params.id);
+
+    return (
+        <Post post={post} />
+    );
+}
