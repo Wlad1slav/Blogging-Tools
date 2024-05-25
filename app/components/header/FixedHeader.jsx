c42ca34d92f7532ee6dc622b2527399466a2f0eb
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-05-25 10:30:28 +0300

    Better organization of code in the header

--- app/components/header/FixedHeader.jsx
+++ app/components/header/FixedHeader.jsx
@@ -1,8 +1,15 @@
 import './styles/fixed.scss';
 import {Avatar} from "@/app/components/header/Avatar";
 
+/**
+ * A fixed header on top that doesn't take up much space.
+ * Appears when the user scrolls down the page.
+ *
+ * @param isDisplayed
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function FixedHeader({isDisplayed}) {
-    // TopHeader when scrolling down
     return (
         <header className='fixed' style={!isDisplayed ? {display: "none"} : {}}>
             <a href="/">
--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,44 +1,47 @@
 'use client'
 
-import {useEffect, useState} from "react";
+import {useEffect, useState, useCallback} from "react";
 import axios from "axios";
 
 import {FixedHeader} from "./FixedHeader";
 import {TopHeader} from "./TopHeader";
 
+/**
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function Header() {
-
     const [showFixedHeader, setShowFixedHeader] = useState(false);
     const [blogInfo, setBlogInfo] = useState({});
 
-    const handleScroll = () => {
-        if (window.scrollY > 350) {
-            setShowFixedHeader(true);
-        } else if (window.scrollY === 0) {
-            setShowFixedHeader(false);
+    const handleScroll = useCallback(() => {
+        setShowFixedHeader(window.scrollY > 350);
+    }, []);
+
+    const fetchBlogInfo = async () => {
+        const apiRequest = `${process.env.NEXT_PUBLIC_API_BASE_URL}/personal_information`;
+
+        try {
+            const response = await axios.get(apiRequest);
+            response.data.links = JSON.parse(response.data.links); // Converting a json string to an object
+            setBlogInfo(response.data);
+        } catch (error) {
+            console.error(error);
         }
     };
 
     useEffect(() => {
-        // Set the action when the viewer scrolls the page
         window.addEventListener('scroll', handleScroll);
+        fetchBlogInfo();
 
-        // Get blog info via api
-        axios.get(process.env.NEXT_PUBLIC_API_BASE_URL + '/personal_information')
-            .then(async response => {
-                response.data.links = JSON.parse(response.data.links); // Converting a json string to an object
-                setBlogInfo(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [handleScroll]);
 
     return (
         <>
-            {/*{showFixedHeader ? <FixedHeader /> : <TopHeader /> }*/}
             <FixedHeader isDisplayed={showFixedHeader} info={blogInfo} />
             <TopHeader info={blogInfo} />
         </>
