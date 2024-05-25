172c3aa017a282c78c5b6bd0c5119a25287a9bce
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-05-25 10:31:27 +0300

    Until the information from the database about the profile has been pulled, the terms "Loading..." are displayed.

--- app/components/header/TopHeader.jsx
+++ app/components/header/TopHeader.jsx
@@ -1,22 +1,30 @@
 import './styles/top.scss';
 import {Avatar} from "@/app/components/header/Avatar";
 
+
+/**
+ * The top, large header that contains all the information about the page
+ *
+ * @param info
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function TopHeader({info}) {
 
     // Converting the blog description to several paragraphs
     const description = info && info.description ? info.description.split('\n').map((paragraph, index) =>
         <span key={index}>{paragraph}</span>
-    ) : '';
+    ) : 'Loading...';
 
     // Converting the bio to several paragraphs
     const bio = info && info.bio ? info.bio.split('\n').map((paragraph, index) =>
         <span key={index}>{paragraph}</span>
-    ) : '';
+    ) : 'Loading...';
 
     // Converting an object with links to an ul-list of links
     const links = info && info.links ? Object.keys(info.links).map((key) => (
         <li key={key}><a href={info.links[key]}>{key}</a></li>
-    )) : '';
+    )) : 'Loading...';
 
     return (
         <header>
@@ -25,7 +33,7 @@ export function TopHeader({info}) {
             </a>
 
             <div className="personal-info">
-                <h1>{info.nickname}</h1>
+                <h1>{info.nickname ?? 'Loading...'}</h1>
 
                 <p className="blog-description">
                     {description}
